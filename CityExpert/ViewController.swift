a61fb37b14acdca97e1787db58f9a4765a7728b7
Zakariya Kassim <zakariyakassim@techs-macbook-pro.local>
2016-06-14 02:05:34 +0100

    Initial Commit

---
+++ CityExpert/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CityExpert
+//
+//  Created by Zakariya Kassim on 14/06/2016.
+//  Copyright © 2016 Zakariya Kassim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
